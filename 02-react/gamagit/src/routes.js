f68772868228905337de7bfbe08a199c41a16629
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-05-18 16:58:42 -0300

    Configurando rota principal

--- 02-react/gamagit/src/routes.js
+++ 02-react/gamagit/src/routes.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {BrowserRouter,
+  Routes, // instead of "Switch"
+  Route,} from "react-router-dom";
 
 import Repositories from "./Repositories";
+import Home from "./Home";
 
-export default function Routes() {
+export default function Rotas() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/repositories" component={Repositories} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/repositories" element={<Repositories />} />
+      </Routes>
     </BrowserRouter>
   );
 }
